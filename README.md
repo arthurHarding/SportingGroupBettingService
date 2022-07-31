173faa6827268ce9cce6db7fbd030eea793db30f
Arthur Harding <arthurh1@hotmail.co.uk>
2022-07-31 12:56:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SportingGroupBettingService
+ Betting and fixture service API
---
+++ SportingGroupBettingService/Controllers/FixtureController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SportingGroupBettingService.Models;
+
+namespace SportingGroupBettingService.Controllers
+{
+      [ApiController]
+      [Route("[controller]")]
+      public class FixtureController : ControllerBase
+      {
+            private readonly ILogger<FixtureController> _logger;
+
+            public FixtureController(ILogger<FixtureController> logger)
+            {
+                  _logger = logger;
+            }
+
+            [HttpGet]
+            public IEnumerable<Fixture> Get()
+            {
+                  
+            }
+      }
+}
